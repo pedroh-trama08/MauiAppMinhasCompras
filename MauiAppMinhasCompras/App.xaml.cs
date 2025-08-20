f6e33efe4b123f2bd1abf8d1722697f83a19e2d8
pedro_twjgqs5 <pedro_twjgqs5@PEDRO>
2025-08-20 16:03:30 -0300

    Criação da interface e inserção do produto

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,7 +1,28 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db; //Campo - onde os dados estão gravados
+
+        public static SQLiteDatabaseHelper Db //Propriedade - forma de acesso ao campo
+        {
+            get
+            {
+                if(_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData), 
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+        } 
         public App()
         {
             InitializeComponent();
